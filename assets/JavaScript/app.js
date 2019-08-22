4cf12970db040d2d35144f6043e36aa65d866bdf
Nathan Rendon <naterendon@gmail.com>
2019-08-22 17:22:07 -0500

    added buttons and a few more divs

--- assets/JavaScript/app.js
+++ assets/JavaScript/app.js
@@ -1,1 +1,9 @@
-let questions = [ "", "", "", "", "", "", "", ""]
+let questions = [ "If you have one pound of clay and one pound of steel, then does steel weighs more than the clay?", 
+"If pizza arrives at a party, then would it be pizza time? or party time?", 
+"", "", "", "", "", ""];
+let answer1 = [ "true", "pizza time", "true", "true", 
+                "true", "true", "true", "true"];
+let answer2 = [ "false", "party time", "false", "false", 
+"false", "false", "false", "false"];
+    
+
