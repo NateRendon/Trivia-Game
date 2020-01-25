1b6fee5856a4419c053585b7b46b4a30805523eb
Nathan Rendon <naterendon@gmail.com>
2020-01-24 20:20:48 -0600

    added a new backgroud picture and changed some questions in the game

--- assets/JavaScript/app.js
+++ assets/JavaScript/app.js
@@ -1,124 +1,153 @@
-$(document).ready(function() {
-    // Create a function that creates the start button and initial screen
-    
-    function initialScreen() {
-        startScreen = "<p class='text-center main-button-container'><a class='btn btn-primary btn-lg btn-block start-button' href='#' role='button'>Start Quiz</a></p>";
-        $(".mainArea").html(startScreen);
+const listOfQuestions = [
+    {question: "Sega came out with a very iconic hedgehog that has become fairly renown. what is his name??", answer1: "Tails", answer2: "Sonic", answer3: "Knuckles", answer4: "Eggman", finalAttr: "answer2", finalText: "Sonic"},
+    {question: "In Legend of Zelda, what is the name of the warrior who takes up the legendary Master Sword??", answer1: "Epona", answer2: "Link", answer3: "Tingle", answer4: "Zelda", finalAttr: "answer2", finalText: "Link"},
+    {question: "In Nintendo's DK series. DK is the initials for the main character. What is his name??", answer1: "Donkey Kong", answer2: "Daddy Kong", answer3: "Diddy Kong", answer4: "Dankey Kong", finalAttr: "answer1", finalText: "Donkey Kong"},
+    {question: "?", answer1: "", answer2: "", answer3: "", answer4: "", finalAttr: "answer3", finalText: ""},
+    {question: "?", answer1: "", answer2: "", answer3: "", answer4: "", finalAttr: "answer1", finalText: ""},
+    {question: "?", answer1: "", answer2: "", answer3: "", answer4: "", finalAttr: "answer4", finalText: ""},
+    {question: "?", answer1: "", answer2: "", answer3: "", answer4: "", finalAttr: "answer2", finalText: ""},
+    {question: "?", answer1: "", answer2: "", answer3: "", answer4: "", finalAttr: "answer1", finalText: ""},
+    {question: "?", answer1: "", answer2: "", answer3: "", answer4: "", finalAttr: "answer2", finalText: ""},
+    {question: "Our beloved pink ball Kirby fought many bosses, who was normally the firs to fight kirby??", answer1: "Marx", answer2: "MetaKnight", answer3: "Whispy Woods", answer4: "King DeDeDe", finalAttr: "answer3", finalText: "Whispy Woods"},
+]
+
+let intervalId = 0;
+let questionId = 0;
+let timeRemaining = 20;
+let totalWins = 0;
+let totalLosses = 0;
+let totalTimeouts = 0;
+
+function startTrivia() {
+    displayQuestion();
+    startTimer();
+}
+
+function displayQuestion() {
+    $('#question').text(listOfQuestions[questionId].question);
+    $('#answer1').text(listOfQuestions[questionId].answer1);
+    $('#answer2').text(listOfQuestions[questionId].answer2);
+    $('#answer3').text(listOfQuestions[questionId].answer3);
+    $('#answer4').text(listOfQuestions[questionId].answer4);
+}
+
+function startTimer() {
+    timeRemaining = 20;
+    $('#time-remaining').text(`Time Remaining: ${timeRemaining}`);
+
+    //Display timer every second
+    intervalId = setInterval(checkTimeRemaining, 1000);
+}
+
+function checkTimeRemaining() {
+    if (timeRemaining > 1) {
+        calculateTimeRemaining();
     }
-    
-    initialScreen();
-    
-    //Create a function, generateHTML(), that is triggered by the start button, and generates the HTML seen on the project video...
-    
-    $("body").on( function(){
-        event.preventDefault();  // added line to test issue on GitHub Viewer
-        generateHTML();
-    
-        timerWrapper();
-    
-    }); // Closes start-button click
-    
-    $("body").on("click", ".answer", function(event){
-        //answeredQuestion = true;
-        clickSound.play();
-        selectedAnswer = $(this).text();
-        if(selectedAnswer === correctAnswers[questionCounter]) {
-            //alert("correct");
-    
-            clearInterval(theClock);
-            generateWin();
-        }
-        else {
-            //alert("wrong answer!");
-            clearInterval(theClock);
-            generateLoss();
-        }
-    }); // Close .answer click
-    
-    $("body").on("click", ".reset-button", function(event){
-        clickSound.play();
-        resetGame();
-    }); // Closes reset-button click
-    
-    });  //  Closes jQuery wrapper
-    
-    function generateLossDueToTimeOut() {
-        unansweredTally++;
-        gameHTML = "<p class='text-center timer-p'>Time Remaining: <span class='timer'>" + counter + "</span></p>" + "<p class='text-center'>You ran out of time!  The correct answer was: " + correctAnswers[questionCounter] + "</p>" + "<img class='center-block img-wrong' src='img/x.png'>";
-        $(".mainArea").html(gameHTML);
-        setTimeout(wait, 4000);  //  change to 4000 or other amount
+    //Out of Time
+    else {
+        calculateTimeRemaining();
+        displayAnswer(false, "OUT OF TIME!");
+        displayNextQuestion();
+        totalTimeouts++;
     }
-    
-    function generateWin() {
-        correctTally++;
-        gameHTML = "<p class='text-center timer-p'>Time Remaining: <span class='timer'>" + counter + "</span></p>" + "<p class='text-center'>Correct! The answer is: " + correctAnswers[questionCounter] + "</p>" + imageArray[questionCounter];
-        $(".mainArea").html(gameHTML);
-        setTimeout(wait, 4000);  //  change to 4000 or other amount
+}
+
+function calculateTimeRemaining() {
+    timeRemaining--;
+    $('#time-remaining').text(`Time Remaining: ${timeRemaining}`);
+}
+
+function verifyAnswerChosen(answerClicked) {
+    //Correct Answer
+    if (listOfQuestions[questionId].finalAttr === answerClicked) {
+        displayAnswer(true, "CORRECT!");
+        totalWins++;
     }
-    
-    function generateLoss() {
-        incorrectTally++;
-        gameHTML = "<p class='text-center timer-p'>Time Remaining: <span class='timer'>" + counter + "</span></p>" + "<p class='text-center'>Wrong! The correct answer is: "+ correctAnswers[questionCounter] + "</p>" + "<img class='center-block img-wrong' src='img/x.png'>";
-        $(".mainArea").html(gameHTML);
-        setTimeout(wait, 4000); //  change to 4000 or other amount
+    //Wrong Answer
+    else {
+        displayAnswer(false, "WRONG!");
+        totalLosses++;
     }
-    
-    function generateHTML() {
-        gameHTML = "<p class='text-center timer-p'>Time Remaining: <span class='timer'>30</span></p><p class='text-center'>" + questionArray[questionCounter] + "</p><p class='first-answer answer'>A. " + answerArray[questionCounter][0] + "</p><p class='answer'>B. "+answerArray[questionCounter][1]+"</p><p class='answer'>C. "+answerArray[questionCounter][2]+"</p><p class='answer'>D. "+answerArray[questionCounter][3]+"</p>";
-        $(".mainArea").html(gameHTML);
+}
+
+function displayAnswer(isCorrect, resultText) {
+    stopTimer();
+    clearQuestion();
+    $('#question').text(`${resultText}`);
+    if (!isCorrect) {
+        $('#answer1').text(`The Correct Answer is: ${listOfQuestions[questionId].finalText}`);
     }
-    
-    function wait() {
-        if (questionCounter < 7) {
-        questionCounter++;
-        generateHTML();
-        counter = 30;
-        timerWrapper();
-        }
-        else {
-            finalScreen();
-        }
+}
+
+function stopTimer() {
+    clearInterval(intervalId);
+}
+
+function clearQuestion() {
+    $('#question').empty();
+    $('#answer1').empty();
+    $('#answer2').empty();
+    $('#answer3').empty();
+    $('#answer4').empty();
+}
+
+function displayNextQuestion() {
+    questionId++;
+
+    //Wait three seconds before moving to next screen
+    if (questionId !== listOfQuestions.length) {
+        let timeoutId = setTimeout(startTrivia, 1000 * 3);
     }
-    
-    function timerWrapper() {
-        theClock = setInterval(thirtySeconds, 1000);
-        function thirtySeconds() {
-            if (counter === 0) {
-                clearInterval(theClock);
-                generateLossDueToTimeOut();
-            }
-            if (counter > 0) {
-                counter--;
-            }
-            $(".timer").html(counter);
-        }
+    else {
+        let timeoutId = setTimeout(displayStats, 1000 * 3);
     }
-    
-    function finalScreen() {
-        gameHTML = "<p class='text-center timer-p'>Time Remaining: <span class='timer'>" + counter + "</span></p>" + "<p class='text-center'>All done, here's how you did!" + "</p>" + "<p class='summary-correct'>Correct Answers: " + correctTally + "</p>" + "<p>Wrong Answers: " + incorrectTally + "</p>" + "<p>Unanswered: " + unansweredTally + "</p>" + "<p class='text-center reset-button-container'><a class='btn btn-primary btn-lg btn-block reset-button' href='#' role='button'>Reset The Quiz!</a></p>";
-        $(".mainArea").html(gameHTML);
-    }
-    
-    function resetGame() {
-        questionCounter = 0;
-        correctTally = 0;
-        incorrectTally = 0;
-        unansweredTally = 0;
-        counter = 30;
-        generateHTML();
-        timerWrapper();
-    }
-    
-    var startScreen;
-    var gameHTML;
-    var counter = 30;
-    var questionArray = ["What is the capital of Australia?", "What is the capital of Liberia?", "What is the capital of Taiwan?", "What is the capital of Japan?", "What is the capital of China?", "What is the capital of Turkey?", "What is the capital of Colombia?", "What is the capital of India?"];
-    var answerArray = [["Canberra", "Melbourne", "Sydney", "Darwin"], ["Arthington","Monrovia","Tuzon","Marshall"], ["Tainan City", "Taichung", "Taipei", "Hsinchu"], ["Kyoto","Hiroshima","Tokyo","Osaka"], ["Hong Kong", "Macau", "Shanghai", "Beijing"], ["Ankara","Istanbul","Antalya","Bursa"], ["Medellin", "Bogota", "Cartagena", "Cali"], ["Mumbai","Hyderabad","Bangalore","New Delhi"]];
-    var imageArray = ["<img class='center-block img-right' src='img/australia.png'>", "<img class='center-block img-right' src='img/liberia.png'>", "<img class='center-block img-right' src='img/taiwan.png'>", "<img class='center-block img-right' src='img/japan.png'>", "<img class='center-block img-right' src='img/china.png'>", "<img class='center-block img-right' src='img/turkey.png'>", "<img class='center-block img-right' src='img/colombia.png'>", "<img class='center-block img-right' src='img/india.png'>"];
-    var correctAnswers = ["A. Canberra", "B. Monrovia", "C. Taipei", "C. Tokyo", "D. Beijing", "A. Ankara", "B. Bogota", "D. New Delhi"];
-    var questionCounter = 0;
-    var selecterAnswer;
-    var theClock;
-    var correctTally = 0;
-    var incorrectTally = 0;
-    var unansweredTally = 0;
+}
+
+function displayStats() {
+    $('#question').text("Finished! Here's your final score");
+    $('#answer1').text(`Correct Answers: ${totalWins}`);
+    $('#answer2').text(`Wrong Answers: ${totalLosses}`);
+    $('#answer3').text(`Unanswered Questions: ${totalTimeouts}`);
+    $('#answer4').empty();
+    $('#playAgain').removeClass("invisible");
+}
+
+function resetStats() {
+    intervalId = 0;
+    questionId = 0;
+    timeRemaining = 20;
+    totalWins = 0;
+    totalLosses = 0;
+    totalTimeouts = 0;
+    $('#playAgain').addClass("invisible");
+}
+
+$('#start').on("click", function () {
+    $('#startButton').hide();
+    startTrivia();
+});
+
+$('#answer1').on("click", function () {
+    verifyAnswerChosen("answer1");
+    displayNextQuestion();
+});
+
+$('#answer2').on("click", function () {
+    verifyAnswerChosen("answer2");
+    displayNextQuestion();
+});
+
+$('#answer3').on("click", function () {
+    verifyAnswerChosen("answer3");
+    displayNextQuestion();
+});
+
+$('#answer4').on("click", function () {
+    verifyAnswerChosen("answer4");
+    displayNextQuestion();
+});
+
+$('#playAgain').on("click", function () {
+    resetStats();
+    startTrivia();
+});
